0313d7a7ea537c0490a30f6362c0dead107aeb32
ayouboul21 <oulahraiouu@gmail.com>
2024-08-01 13:18:01 +0100

    norm all good

---
+++ .print.c
@@ -0,0 +1,21 @@
+#include "cub3d.h"
+
+void	printer(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	printf("NO :%s\n", map->north);
+	printf("SO :%s\n", map->south);
+	printf("WE :%s\n", map->west);
+	printf("EA :%s\n", map->east);
+	printf("FLOOR :%s\n", map->floor_color);
+	printf("CEILING :%s", map->ceiling_color);
+	printf("MAP :\n");
+	while (map->map[i])
+	{
+		printf("%s\n", map->map[i]);
+		i++;
+	}
+	printf("player x : %d player y : %d\n", map->player.x, map->player.y);
+}
